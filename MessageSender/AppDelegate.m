6666d56e093f74ddaa1c5fd3e76cd01b73fb305d
Neil Singh <Neil@neils-macbook-pro.local>
2016-08-30 17:23:00 -0500

    Initial Commit

---
+++ MessageSender/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  MessageSender
+//
+//  Created by Neil Singh on 8/30/16.
+//  Copyright © 2016 Neil Singh. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
---
+++ MessageSender/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MessageSender
+//
+//  Created by Neil Singh on 8/30/16.
+//  Copyright © 2016 Neil Singh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
